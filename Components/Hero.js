45d974389ba6f3539a1798c8f9c3f4b74575e60a
DESKTOP-4J5ID3D\anbon <namoon0608@gmail.com>
2022-08-15 15:35:56 +0800

    0815-3

--- Components/Hero.js
+++ Components/Hero.js
@@ -1,7 +1,6 @@
-import Image from "next/image";
 import styles from "../styles/Hero.module.scss";
 
-export default function Hero() {
+export default function Hero(props) {
     return (
         <div>
             <div className={styles.bgWrap}>
@@ -13,13 +12,18 @@ export default function Hero() {
                     quality={100}
                 />
             </div>
-            <div className={styles.upperTab}>
-                <h1>2022台北國際食品展</h1>
-                <ul>
-                    <li>2022年5月13日(含)前完成申請，可享 9折優惠。</li>
-                    <li>2022年5月14日至6月2日申請者按標準收費。</li>
-                    <li>2022年6月6日(含)以後須加付逾期申請費50%。</li>
-                </ul>
+            <div className={styles.main}>
+                <div className={styles.tabContent}>
+                    <div className={styles.upperTab}>
+                        <h1>2022台北國際食品展</h1>
+                        <ul>
+                            <li>2022年5月13日(含)前完成申請，可享 9折優惠。</li>
+                            <li>2022年5月14日至6月2日申請者按標準收費。</li>
+                            <li>2022年6月6日(含)以後須加付逾期申請費50%。</li>
+                        </ul>
+                    </div>
+                </div>
+                <div className={styles.content}>{props.children}</div>
             </div>
         </div>
     );
--- Components/Nav.js
+++ Components/Nav.js
@@ -67,6 +67,25 @@ export default function Nav(props) {
                     </ul>
                 </div> */}
             </div>
+            <div className={styles.mobileNav}>
+                <Link href={"/"}>
+                    <a className={styles.logo}>
+                        <img src="/img/logo.svg" alt="TAITRA" />
+                        <p>專業展水電線上申請</p>
+                    </a>
+                </Link>
+                <svg
+                    width="25"
+                    height="17"
+                    viewBox="0 0 25 17"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                >
+                    <rect width="25" height="3" fill="#225183" />
+                    <rect y="7" width="25" height="3" fill="#225183" />
+                    <rect y="14" width="25" height="3" fill="#225183" />
+                </svg>
+            </div>
         </nav>
     );
 }
