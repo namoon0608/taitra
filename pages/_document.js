4476e591e3f52e1afa0955787cbcc7eb2a8ab59a
Michelle Chiang <namoon0608@gmail.com>
2022-08-26 19:27:55 +0800

    0826-7

--- pages/_document.js
+++ pages/_document.js
@@ -8,10 +8,6 @@ class MyDocument extends Document {
                         rel="stylesheet"
                         href="https://fonts.googleapis.com/css2?family=Raleway:wght@100;200;300;400;500;600;700;800;900&family=Roboto:wght@100;300;400;500;700;900&display=swap"
                     />
-                    <meta
-                        httpEquiv="Content-Security-Policy"
-                        content="default-src 'self' https: ; object-src 'none'"
-                    />
                 </Head>
                 <body>
                     <Main />
